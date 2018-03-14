1c4864351716a803787466b7cb5ef9d7f09d27ee
Benjamin Grange <benjamin.grange@epitech.eu>
2018-03-14 22:18:37 +0100

    Added more clippy lints for all crates.

--- libnest/src/lib.rs
+++ libnest/src/lib.rs
@@ -1,7 +1,8 @@
 //! Nest backend library
 //!
 //! This crate performs backend operations for Raven's Package Manager, like installation, removal, and search of a package.
-
+//
+// Rustc
 #![warn(missing_docs)]
 #![warn(missing_debug_implementations)]
 #![warn(trivial_casts)]
@@ -10,6 +11,18 @@
 #![warn(unused_extern_crates)]
 #![warn(unused_import_braces)]
 #![warn(unused_qualifications)]
+
+// Clippy
+#![warn(fallible_impl_from)]
+#![warn(float_cmp_const)]
+#![warn(int_plus_one)]
+#![warn(mem_forget)]
+#![warn(mut_mut)]
+#![warn(mutex_integer)]
+#![warn(nonminimal_bool)]
+#![warn(pub_enum_variant_names)]
+#![warn(range_plus_one)]
+#![warn(stutter)]
 
 pub mod config;
 pub mod repository;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+// Rustc
+#![warn(missing_docs)]
+#![warn(missing_debug_implementations)]
+#![warn(trivial_casts)]
+#![warn(trivial_numeric_casts)]
+#![warn(unreachable_pub)]
+#![warn(unused_extern_crates)]
+#![warn(unused_import_braces)]
+#![warn(unused_qualifications)]
+
+// Clippy
+#![warn(fallible_impl_from)]
+#![warn(float_cmp_const)]
+#![warn(int_plus_one)]
+#![warn(mem_forget)]
+#![warn(mut_mut)]
+#![warn(mutex_integer)]
+#![warn(nonminimal_bool)]
+#![warn(pub_enum_variant_names)]
+#![warn(range_plus_one)]
+#![warn(stutter)]
+
 extern crate clap;
 
 mod command;
