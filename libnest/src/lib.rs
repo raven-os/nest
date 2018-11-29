70747d858e4b377f2b7cce66fecc89197eee588c
Benjamin Grange <benjamin.grange@epitech.eu>
2018-11-29 10:59:16 +0000

    Added documentation for crate libnest.

--- libnest/src/lib.rs
+++ libnest/src/lib.rs
@@ -1,2 +1,6 @@
+//! Nest's backend library
+//!
+//! This crate performs backend operations for Raven's Package Manager, like installation, remova of a package.
+
 #![warn(missing_docs)]
 #![warn(missing_debug_implementations)]
