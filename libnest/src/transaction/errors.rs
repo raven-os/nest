db6be4f00d174a3a13704bc17ca14ee76620b70e
Doom <clement.doumergue@epitech.eu>
2019-01-31 13:11:32 -0500

    Add the remove transaction to libnest

--- libnest/src/transaction/errors.rs
+++ libnest/src/transaction/errors.rs
@@ -21,3 +21,27 @@ pub enum InstallErrorKind {
 }
 
 use_as_error!(InstallError, InstallErrorKind);
+
+/// Error type for errors related to package removal
+#[derive(Debug)]
+pub struct RemoveError {
+    inner: Context<RemoveErrorKind>,
+}
+
+/// Error kind describing a kind of error related to package removal
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Fail)]
+pub enum RemoveErrorKind {
+    /// The package could not be removed because its log file could not be loaded
+    #[fail(display = "log file not found")]
+    LogFileLoadError,
+
+    /// The package could not be completely removed because one of its files could not be removed
+    #[fail(display = "cannot remove package file")]
+    FileRemoveError,
+
+    /// The package could not be completely removed because its log file could not be removed
+    #[fail(display = "cannot remove log file")]
+    LogFileRemoveError,
+}
+
+use_as_error!(RemoveError, RemoveErrorKind);
