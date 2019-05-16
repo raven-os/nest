1ca603e1a72edeff00792d6aecd74484aa3cfe05
Doom <clement.doumergue@epitech.eu>
2019-05-16 23:22:32 +0200

    Add shell-searching before executing instructions.sh

--- libnest/src/transaction/instructions.rs
+++ libnest/src/transaction/instructions.rs
@@ -1,8 +1,11 @@
 use std::fs::File;
 use std::io::Read;
-
-use super::errors::{InstructionsExecutionError, InstructionsExecutionErrorKind};
+use std::path::Path;
 use std::process::Command;
+
+use crate::chroot::Chroot;
+
+use super::errors::{InstructionsExecutionError, InstructionsExecutionErrorKind::*};
 
 /// Type representing the result of an execution
 /// It contains fields for the exit status, stdout, and stderr
@@ -15,6 +18,30 @@ pub struct InstructionsExecutor {
 }
 
 impl InstructionsExecutor {
+    fn find_suitable_shell(root: &Path) -> Option<std::path::PathBuf> {
+        let shells = [Path::new("/bin/sh"), Path::new("/bin/bash")];
+
+        shells.iter().find_map(|shell| {
+            let chrooted_shell = shell.with_root(root);
+
+            if !chrooted_shell.exists() {
+                return None;
+            }
+
+            let mut cmd = Command::new(chrooted_shell);
+            cmd.arg("-c");
+            cmd.arg(":");
+
+            cmd.output().ok().and_then(|output| {
+                if output.status.success() {
+                    Some(shell.to_path_buf())
+                } else {
+                    None
+                }
+            })
+        })
+    }
+
     /// Creates an [`InstructionsExecutor`] from a script file
     pub fn from_script_file(
         file: &mut File,
@@ -22,7 +49,7 @@ impl InstructionsExecutor {
         let mut script_source = String::new();
 
         file.read_to_string(&mut script_source)
-            .map_err(|_| InstructionsExecutionErrorKind::CannotReadInstructions)?;
+            .map_err(|_| CannotReadInstructions)?;
 
         Ok(Self { script_source })
     }
@@ -30,21 +57,20 @@ impl InstructionsExecutor {
     fn execute_function(
         &self,
         func_name: &str,
-        root: &std::path::Path,
+        root: &Path,
     ) -> Result<ExecutionOutput, InstructionsExecutionError> {
+        let shell = Self::find_suitable_shell(root).ok_or(CannotFindShell)?;
         let mut cmd = Command::new("chroot");
 
         cmd.arg(root.display().to_string());
-        cmd.arg("/bin/sh"); // TODO: find a shell dynamically
+        cmd.arg(shell);
         cmd.arg("-c");
         cmd.arg(format!("{}\n{}", self.script_source, func_name));
 
-        let output = cmd
-            .output()
-            .map_err(|_| InstructionsExecutionErrorKind::CannotExecuteShell)?;
+        let output = cmd.output().map_err(|_| CannotExecuteShell)?;
 
         if !output.status.success() {
-            Err(InstructionsExecutionErrorKind::FailureExitStatus(output).into())
+            Err(FailureExitStatus(output).into())
         } else {
             Ok(output)
         }
@@ -53,7 +79,7 @@ impl InstructionsExecutor {
     /// Executes the pre-installation script
     pub fn execute_before_install(
         &self,
-        root: &std::path::Path,
+        root: &Path,
     ) -> Result<ExecutionOutput, InstructionsExecutionError> {
         self.execute_function("before_install", root)
     }
@@ -61,7 +87,7 @@ impl InstructionsExecutor {
     /// Executes the post-installation script
     pub fn execute_after_install(
         &self,
-        root: &std::path::Path,
+        root: &Path,
     ) -> Result<ExecutionOutput, InstructionsExecutionError> {
         self.execute_function("after_install", root)
     }
@@ -69,7 +95,7 @@ impl InstructionsExecutor {
     /// Executes the pre-uninstallation script
     pub fn execute_before_remove(
         &self,
-        root: &std::path::Path,
+        root: &Path,
     ) -> Result<ExecutionOutput, InstructionsExecutionError> {
         self.execute_function("before_remove", root)
     }
@@ -77,7 +103,7 @@ impl InstructionsExecutor {
     /// Executes the post-uninstallation script
     pub fn execute_after_remove(
         &self,
-        root: &std::path::Path,
+        root: &Path,
     ) -> Result<ExecutionOutput, InstructionsExecutionError> {
         self.execute_function("after_remove", root)
     }
